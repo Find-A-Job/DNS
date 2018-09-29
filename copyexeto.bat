8b2c81f41d08812941bfcc0aed0eba753aaf015d
Find-A-Job <1659862580@qq.com>
2018-09-29 17:52:06 +0800

    拷贝文件脚本

---
+++ copyexeto.bat
@@ -0,0 +1,7 @@
+@echo off
+copy D:\vs2015\4-程序\9_28-DNS\9_28-DNS\9_28-DNS.cpp D:\vs2015\4-程序\9_28-DNS\GITHUB\DNS /y
+copy D:\vs2015\4-程序\9_28-DNS\9_28-DNS\9_28-DNS.exe D:\vs2015\4-程序\9_28-DNS\GITHUB\DNS /y
+copy D:\vs2015\4-程序\9_28-DNS\9_28-DNS\Macor.h D:\vs2015\4-程序\9_28-DNS\GITHUB\DNS /y
+copy D:\vs2015\4-程序\9_28-DNS\9_28-DNS\stdafx.cpp D:\vs2015\4-程序\9_28-DNS\GITHUB\DNS /y
+copy D:\vs2015\4-程序\9_28-DNS\9_28-DNS\stdafx.h D:\vs2015\4-程序\9_28-DNS\GITHUB\DNS /y
+copy D:\vs2015\4-程序\9_28-DNS\9_28-DNS\targetver.h D:\vs2015\4-程序\9_28-DNS\GITHUB\DNS /y
